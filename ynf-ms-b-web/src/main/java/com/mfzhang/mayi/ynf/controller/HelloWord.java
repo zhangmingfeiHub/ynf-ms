2a006f8593e3b43b1a7ed12fe94b24ca9ce13d35
mingfei.zhang <409244379@qq.com>
2017-12-17 20:30:57 +0800

    添加测试类

---
+++ ynf-ms-b-web/src/main/java/com/mfzhang/mayi/ynf/controller/HelloWord.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author mingfei.z
+ */
+package com.mfzhang.mayi.ynf.controller;
+
+/**
+ * 
+ * @author mingfei.z
+ *
+ */
+public class HelloWord {
+
+}
---
+++ ynf-ms-b-web/src/main/java/com/mfzhang/mayi/ynf/controller/HelloWord2.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author mingfei.z 2017年12月17日 下午8:29:52
+ */
+package com.mfzhang.mayi.ynf.controller;
+
+/**
+ * 
+ * @author mingfei.z
+ */
+public class HelloWord2 {
+
+}
